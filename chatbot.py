369606e8283f8c12bb33476e8628e3d5c90fbf16
Wen-Zeying <251687289@qq.com>
2024-03-10 23:24:21 +0800

    secure

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, 
                           CallbackContext)
-import configparser
+#import configparser
 import logging
 import redis
 from ChatGPT_HKBU import HKBU_ChatGPT
@@ -10,12 +11,13 @@ from ChatGPT_HKBU import HKBU_ChatGPT
 global redis1
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    #config = configparser.ConfigParser()
+    #config.read('config.ini')
+    #updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    updater = Updater(token=(os.environ['TELEGRAM_ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-    global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    #global redis1
+    redis1 = redis.Redis(host=(os.environ['REDIS_HOST']), password=(os.environ['REDIS_PASSWORD']), port=(os.environ['REDIS_REDISPORT']))
    
     # You can set this logging module, so you will know when and why things do not work as expected Meanwhile, update your config.ini as:
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -26,14 +28,14 @@ def main():
 
     # dispatcher for chatgpt
     global chatgpt
-    chatgpt = HKBU_ChatGPT(config)
+    #chatgpt = HKBU_ChatGPT(config)
     chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("add", add))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("hello", hello))
+    #dispatcher.add_handler(CommandHandler("add", add))
+    #dispatcher.add_handler(CommandHandler("help", help_command))
+    #dispatcher.add_handler(CommandHandler("hello", hello))
     
     # To start the bot:
     updater.start_polling()
